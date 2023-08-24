1a78070d92919e1063633dfac32c6d74465b9614
潘凌峰 <1946578780@qq.com>
2023-08-24 18:50:37 +0800

    build: 添加tailwind

--- src/views/page/Page.vue
+++ src/views/page/Page.vue
@@ -1,8 +1,9 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    1111111
+  <div class="border-spacing-2 flex items-center bg-red-50 text-xs font-thin border">
+    <div class="flex justify-center items-center gap-5">
+      <span class="text-white px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800">111</span>
+    </div>
   </div>
 </template>
