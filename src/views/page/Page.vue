0a38bea633bcf5a51409ffdb48dd87072ae877c5
潘凌峰 <1946578780@qq.com>
2023-08-25 18:07:59 +0800

    feat: 添加自动导入

--- src/views/page/Page.vue
+++ src/views/page/Page.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { ref } from 'vue'
 import Banner from './cpns/Banner.vue'
 
 const images = ref([
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,37 @@
+import path from 'path'
+
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+import Vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+
 export default defineConfig({
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'assets/js/[name]-[hash].js',
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+      },
+
+      manualChunks(id) {
+        if (id.includes('node_modules')) {
+          return 'vendor'
+        }
+      }
+    }
+  },
+
   plugins: [
-    Vue()
+    Vue(),
+    AutoImport({
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue', 'vue-router'],
+
+      dts: path.resolve('./', 'auto-imports.d.ts')
+    })
   ],
 
   server: {
